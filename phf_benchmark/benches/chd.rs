79238987b2c3e2c62f7f1d6cdeff3fc5f79faf9d
astral4 <88992929+astral4@users.noreply.github.com>
2023-09-08 13:02:15 -0400

    remove repetition

--- phf_benchmark/benches/chd.rs
+++ phf_benchmark/benches/chd.rs
@@ -1,6 +1,6 @@
 use core::hint::black_box;
 use criterion::{criterion_group, criterion_main, BatchSize, Criterion};
-use phf_benchmark::{MAP_CHD_10K, MAP_CHD_1K};
+use phf_benchmark::*;
 use phf_chd::MapGenerator;
 use phf_shared::hash::AHasher;
 use phf_shared::{PhfMap, FIXED_SEED};
@@ -16,7 +16,7 @@ fn eval_chd(c: &mut Criterion) {
     group.bench_function("eval_chd_1k", |b| {
         b.iter_batched(
             || samples.next().unwrap(),
-            |x| black_box(MAP_CHD_1K.get_entry(&x)),
+            |x| black_box(MAP_CHD_1000.get_entry(&x)),
             BatchSize::SmallInput,
         )
     });
@@ -24,7 +24,7 @@ fn eval_chd(c: &mut Criterion) {
     group.bench_function("eval_chd_10k", |b| {
         b.iter_batched(
             || samples.next().unwrap(),
-            |x| black_box(MAP_CHD_10K.get_entry(&x)),
+            |x| black_box(MAP_CHD_10000.get_entry(&x)),
             BatchSize::SmallInput,
         )
     });
@@ -35,29 +35,19 @@ fn eval_chd(c: &mut Criterion) {
 fn build_chd(c: &mut Criterion) {
     let mut group = c.benchmark_group("build_chd");
 
-    group.bench_function("build_chd_1k", |b| {
-        b.iter_batched(
-            || {
-                SmallRng::seed_from_u64(FIXED_SEED)
-                    .sample_iter(Standard)
-                    .take(1000)
-            },
-            |x| black_box(MapGenerator::<u64, u64, AHasher>::from(x)),
-            BatchSize::SmallInput,
-        )
-    });
-
-    group.bench_function("build_chd_10k", |b| {
-        b.iter_batched(
-            || {
-                SmallRng::seed_from_u64(FIXED_SEED)
-                    .sample_iter(Standard)
-                    .take(10000)
-            },
-            |x| black_box(MapGenerator::<u64, u64, AHasher>::from(x)),
-            BatchSize::SmallInput,
-        )
-    });
+    for (name, size) in [("1k", 1000), ("10k", 10000)] {
+        group.bench_function(format!("build_chd_{name}"), |b| {
+            b.iter_batched(
+                || {
+                    SmallRng::seed_from_u64(FIXED_SEED)
+                        .sample_iter(Standard)
+                        .take(size)
+                },
+                |x| black_box(MapGenerator::<u64, u64, AHasher>::from(x)),
+                BatchSize::SmallInput,
+            )
+        });
+    }
 
     group.finish();
 }
--- phf_benchmark/build.rs
+++ phf_benchmark/build.rs
@@ -14,25 +14,17 @@ fn main() -> Result<(), Box<dyn Error>> {
     let path = Path::new(&var("OUT_DIR")?).join("codegen.rs");
     let mut file = BufWriter::new(File::create(&path)?);
 
-    write!(
-        &mut file,
-        "pub const MAP_CHD_1K: Map<u64, u64, AHasher> = {};",
-        MapGenerator::<u64, u64, AHasher>::from(
-            SmallRng::seed_from_u64(FIXED_SEED)
-                .sample_iter(Standard)
-                .take(1000)
-        )
-    )?;
-
-    write!(
-        &mut file,
-        "pub const MAP_CHD_10K: Map<u64, u64, AHasher> = {};",
-        MapGenerator::<u64, u64, AHasher>::from(
-            SmallRng::seed_from_u64(FIXED_SEED)
-                .sample_iter(Standard)
-                .take(10000)
-        )
-    )?;
+    for size in [1000, 10000] {
+        write!(
+            &mut file,
+            "pub const MAP_CHD_{size}: Map<u64, u64, AHasher> = {};",
+            MapGenerator::<u64, u64, AHasher>::from(
+                SmallRng::seed_from_u64(FIXED_SEED)
+                    .sample_iter(Standard)
+                    .take(size)
+            )
+        )?;
+    }
 
     Ok(())
 }
